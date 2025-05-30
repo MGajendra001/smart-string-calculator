269c6cc6142b152909951ff267451dd44597fe92
MGajendra001 <mangajendra001@gmail.com>
2025-05-30 20:07:02 +0530

    Test for empty string returning 0

--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe StringCalculatorService do
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(described_class.add("")).to eq(0)
+    end
+  end
 end
