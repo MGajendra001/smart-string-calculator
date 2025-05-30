a73ef28ef19676778f5de23a1d03e4a3834d9209
MGajendra001 <mangajendra001@gmail.com>
2025-05-30 20:23:20 +0530

    Test for custom delimiter in input string

--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculatorService do
     it 'supports newlines as delimiters' do
       expect(described_class.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiters' do
+      expect(described_class.add("//;\n1;2")).to eq(3)
+    end
   end
 end
