d2d47e0bb00b63d784997bfd832f06a2b5d8c4cb
MGajendra001 <mangajendra001@gmail.com>
2025-05-30 20:25:36 +0530

    Parse and support custom delimiters

--- app/services/string_calculator_service.rb
+++ app/services/string_calculator_service.rb
@@ -2,7 +2,12 @@ class StringCalculatorService
   def self.add(numbers)
     return 0 if numbers.strip.empty?
 
-    numbers.gsub('\n', "\n")
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      custom_delim = delimiter[2]
+      return numbers.split(custom_delim).map(&:to_i).sum
+    end
+
     numbers.split(/,|\n/).map(&:to_i).sum
   end
 end
